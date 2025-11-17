01e4ed9aaae596846c73b2acfdfebdb7d0d5e578
janne6565 <jabbekeipert@gmail.com>
2025-11-17 22:53:06 +0100

    add base location

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
-        <Routes>
+        <Routes location={"/microservice-weaver"}>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
